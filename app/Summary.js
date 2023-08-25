6930c2198ef96d94e5946d4c80868364d6eb0066
Vinay Wasnik <127746998+VinayWasnik@users.noreply.github.com>
2023-08-25 20:36:37 +0530

    Update Summary.js

--- app/Summary.js
+++ app/Summary.js
@@ -1,17 +1,37 @@
-'use client'
-import { useState } from "react";
+import React from "react";
+
 function Summary({ formData }) {
-    return (
-        <div id="summary">
-            <h2>Summary</h2>
-            <p>Form Type: </p>
-            <p>Age: </p>
+  const { formType, age, selectedShow } = formData;
 
-            <p>DC Shows: </p>
-
-            <p>Marvel Shows: </p>
-
-        </div>
-    );
+  return (
+    <div id="summary">
+      <h2>Summary</h2>
+      <p>Form Type: {formType}</p>
+      <p>Age: {age}</p>
+      <p>Selected Show: {selectedShow}</p>
+    </div>
+  );
 }
+
 export default Summary;
+
+
+
+
+// 'use client'
+// import { useState } from "react";
+// function Summary({ formData }) {
+//     return (
+//         <div id="summary">
+//             <h2>Summary</h2>
+//             <p>Form Type: </p>
+//             <p>Age: </p>
+
+//             <p>DC Shows: </p>
+
+//             <p>Marvel Shows: </p>
+
+//         </div>
+//     );
+// }
+// export default Summary;
