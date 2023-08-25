5d785b8be8bea1aa28c1c9986816d344117209c2
Vinay Wasnik <127746998+VinayWasnik@users.noreply.github.com>
2023-08-25 20:33:20 +0530

    Update FormA.js

--- app/FormA.js
+++ app/FormA.js
@@ -1,31 +1,66 @@
-'use client'
-import { useState } from "react";
+import React from "react";
 
-function FormA({ onSubmit, age }) {
-    const handleSubmit = (event) => {
-    };
-    return (
-        <form id="dc" onSubmit={handleSubmit}>
-            <h2>Form A</h2>
-            <label>
-                Select DC Shows:
-                <select  >
-                    <option value="">--Select--</option>
-                    <option value="The Flash">The Flash</option>
-                    <option value="Arrow">Arrow</option>
-                    <option value="Supergirl">Supergirl</option>
-                    <option value="Legends of Tomorrow">Legends of Tomorrow</option>
-                </select>
-            </label>
-            <br />
-            <label>
-                Enter your age:
-                <input type="number" value={age} disabled />
-            </label>
-            <br />
-            <button id="submit-dc" type="submit">Submit</button>
-
-        </form>
-    );
+function FormA({ onSubmit, age, selectedShow, onShowChange }) {
+  return (
+    <form id="dc" onSubmit={onSubmit}>
+      <h2>Form A</h2>
+      <label>
+        Select DC Shows:
+        <select value={selectedShow} onChange={onShowChange}>
+          <option value="">--Select--</option>
+          <option value="The Flash">The Flash</option>
+          <option value="Arrow">Arrow</option>
+          <option value="Supergirl">Supergirl</option>
+          <option value="Legends of Tomorrow">Legends of Tomorrow</option>
+        </select>
+      </label>
+      <br />
+      <label>
+        Enter your age:
+        <input type="number" value={age} disabled />
+      </label>
+      <br />
+      <button id="submit-dc" type="submit" disabled={!selectedShow}>
+        Submit
+      </button>
+    </form>
+  );
 }
+
 export default FormA;
+
+
+
+
+
+// 'use client'
+// import { useState } from "react";
+
+// function FormA({ onSubmit, age }) {
+//     const handleSubmit = (event) => {
+//     };
+//     return (
+//         <form id="dc" onSubmit={handleSubmit}>
+//             <h2>Form A</h2>
+//             <label>
+//                 Select DC Shows:
+//                 <select  >
+//                     <option value="">--Select--</option>
+//                     <option value="The Flash">The Flash</option>
+//                     <option value="Arrow">Arrow</option>
+//                     <option value="Supergirl">Supergirl</option>
+//                     <option value="Legends of Tomorrow">Legends of Tomorrow</option>
+//                 </select>
+//             </label>
+//             <br />
+//             <label>
+//                 Enter your age:
+//                 <input type="number" value={age} disabled />
+//             </label>
+//             <br />
+//             <button id="submit-dc" type="submit">Submit</button>
+
+//         </form>
+//     );
+// }
+// export default FormA;
